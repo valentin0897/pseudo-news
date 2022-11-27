8c12a6c3b18384aae4e42d18b3ea3ea0440c0a5a
brewbard <08valentin0897@gmail.com>
2022-11-27 15:18:44 +0400

    Remove links from header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,13 +6,11 @@ import { Link } from 'src/app/models/link';
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
-  links: Link[] = [];
   title: string = ""
 
   constructor() { }
 
   ngOnInit(): void {
-    this.links = [new Link("Health", "health"), new Link("Show business", "business"), new Link("Events", "event")];
     this.title = "News of the week"
   }
 
