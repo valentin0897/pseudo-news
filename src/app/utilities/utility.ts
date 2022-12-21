5ccb13f8efb580b0627fc1605f7761c65af57600
brewbard <08valentin0897@gmail.com>
2022-12-21 20:57:42 +0400

    Delete all auto tests files

--- src/app/utilities/utility.ts
+++
@@ -1,8 +0,0 @@
-export function isSmallNews(index: number, numberMediumNews: number, numberSmallNews: number) {
-    let pageNewsTotal = numberMediumNews + numberSmallNews
-
-    if (index % pageNewsTotal < numberSmallNews){
-      return true
-    } 
-    return false
-  }
